7a85b49aecadc00a254fe6d93013ca89be48cc0f
Abdul-asa <bigtulahi1@gmaiil.com>
2024-04-15 23:25:40 +0100

    Set DB from local to Turso

--- db/intialize.ts
+++ db/intialize.ts
@@ -3,13 +3,13 @@ import { drizzle } from "drizzle-orm/libsql";
 import * as schema from "./schema";
 import { createClient } from "@libsql/client";
 
-// const client = createClient({
-//   url: process.env.TURSO_DB_URL || "",
-//   authToken: process.env.TURSO_AUTH_TOKEN,
-// });
-
 const client = createClient({
-  url: "file:./db/local.sqlite",
+  url: process.env.TURSO_DB_URL || "",
+  authToken: process.env.TURSO_AUTH_TOKEN,
 });
+
+// const client = createClient({
+//   url: "file:./db/local.sqlite",
+// });
 
 export const db = drizzle(client, { schema });
--- drizzle.config.ts
+++ drizzle.config.ts
@@ -9,9 +9,9 @@ export default {
   schema: "./db/schema.ts",
   driver: "turso",
   dbCredentials: {
-    url: "file:./db/local.sqlite",
-    // url: process.env.TURSO_DB_URL || "",
-    // authToken: process.env.TURSO_AUTH_TOKEN,
+    // url: "file:./db/local.sqlite",
+    url: process.env.TURSO_DB_URL || "",
+    authToken: process.env.TURSO_AUTH_TOKEN,
   },
   out: "./db/migrations",
   verbose: true,
