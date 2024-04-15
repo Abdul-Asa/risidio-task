9b3d4f579570e11a5be38f1fc1cafa95d418b291
Abdul-asa <bigtulahi1@gmaiil.com>
2024-04-16 00:29:52 +0100

    Update database configuration and logging

--- db/intialize.ts
+++ db/intialize.ts
@@ -3,6 +3,9 @@ import { drizzle } from "drizzle-orm/libsql";
 import * as schema from "./schema";
 import { createClient } from "@libsql/client";
 
+console.log("TURSO_DB_URL", process.env.TURSO_DB_URL);
+console.log("TURSO_AUTH_TOKEN", process.env.TURSO_AUTH_TOKEN);
+
 const client = createClient({
   url: process.env.TURSO_DB_URL || "",
   authToken: process.env.TURSO_AUTH_TOKEN,
